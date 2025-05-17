173e4f5cfe7b004779294d464af46e084dd3a37a
Luong Hoang <luongbs01@gmail.com>
2025-05-17 09:44:06 +0700

    Leetcode 16/05: 1-D Dynamic Programming

--- src/MaximumProductSubarray.java
+++ src/MaximumProductSubarray.java
@@ -3,6 +3,28 @@
  */
 public class MaximumProductSubarray {
     public int maxProduct(int[] nums) {
-        return 0;
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] minNegative = new int[length];
+        int[] maxPositive = new int[length];
+        int ans = nums[0];
+        if (nums[0] > 0) {
+            maxPositive[0] = nums[0];
+        } else {
+            minNegative[0] = nums[0];
+        }
+        for (int i = 1; i < length; i++) {
+            if (nums[i] > 0) {
+                minNegative[i] = minNegative[i - 1] * nums[i];
+                maxPositive[i] = Math.max(nums[i], maxPositive[i - 1] * nums[i]);
+            } else if (nums[i] < 0) {
+                minNegative[i] = Math.min(nums[i], maxPositive[i - 1] * nums[i]);
+                maxPositive[i] = minNegative[i - 1] * nums[i];
+            }
+            ans = Math.max(ans, maxPositive[i]);
+        }
+        return ans;
     }
 }
