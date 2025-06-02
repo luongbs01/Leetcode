7d1f7a32bf0d994c9889a7023aaa469cea77fd3f
Luong Hoang <luongbs01@gmail.com>
2025-06-02 11:33:58 +0700

    Leetcode daily 02/06/2025

---
+++ src/Candy.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * Description: https://leetcode.com/problems/candy/description/
+ */
+
+public class Candy {
+
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += candies[i];
+        }
+        return ans;
+    }
+}
