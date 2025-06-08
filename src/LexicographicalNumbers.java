971da603cd43c60a945e777776894660e07d15e1
Luong Hoang <luongbs01@gmail.com>
2025-06-08 21:16:00 +0700

    Leetcode daily 08/06/2025

---
+++ src/LexicographicalNumbers.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LexicographicalNumbers {
+
+    List<Integer> ans;
+
+    public List<Integer> lexicalOrder(int n) {
+        ans = new ArrayList<>(n);
+        for (int i = 1; i < 10; i++) {
+            gen(i, n);
+        }
+        return ans;
+    }
+
+    private void gen(int i, int n) {
+        if (i <= n) {
+            ans.add(i);
+            for (int j = 0; j < 10; j++) {
+                gen(i * 10 + j, n);
+            }
+        }
+    }
+}
