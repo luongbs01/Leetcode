8daf126227140636bd3e573b4574e87f11c09ca7
Luong Hoang <luongbs01@gmail.com>
2025-06-15 16:09:32 +0700

    Leetcode daily 15/06/2025

---
+++ src/MaxDifferenceYouCanGetFromChangingAnInteger.java
@@ -0,0 +1,28 @@
+/**
+ * Description: https://leetcode.com/problems/max-difference-you-can-get-from-changing-an-integer//description/
+ */
+
+public class MaxDifferenceYouCanGetFromChangingAnInteger {
+    public int maxDiff(int num) {
+        String numStr = String.valueOf(num);
+        int first = 0;
+        while (first < numStr.length() && numStr.charAt(first) == '9') {
+            first++;
+        }
+        int max = num;
+        if (first < numStr.length()) {
+            max = Integer.parseInt(numStr.replace(numStr.charAt(first), '9'));
+        }
+        first = 0;
+        while (first < numStr.length() && (numStr.charAt(first) == '0' || numStr.charAt(first) == '1')) {
+            first++;
+        }
+        int min = num;
+        if (first == 0) {
+            min = Integer.parseInt(numStr.replace(numStr.charAt(first), '1'));
+        } else if (first < numStr.length()) {
+            min = Integer.parseInt(numStr.replace(numStr.charAt(first), '0'));
+        }
+        return max - min;
+    }
+}
