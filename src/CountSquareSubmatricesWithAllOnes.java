1fe954d920f0a7503f04e2e009da37c78e8be92b
Luong Hoang <luongbs01@gmail.com>
2025-08-20 21:07:08 +0700

    Leetcode daily 20/08/2025

---
+++ src/CountSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,22 @@
+/**
+ * Description: https://leetcode.com/problems/count-square-submatrices-with-all-ones/description/
+ */
+
+public class CountSquareSubmatricesWithAllOnes {
+
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length, ans = 0;
+        int[][] dp = new int[m][n]; // dp[i][j] number of square whose bottom-right corner at (i,j)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    else dp[i][j] = 1;
+                    ans += dp[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+}
