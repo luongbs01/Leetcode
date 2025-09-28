28d543f483fc4ed9093cf26a176ed3114911075c
Luong Hoang <luongbs01@gmail.com>
2025-09-28 15:43:52 +0700

    Leetcode daily 28/09/2025

---
+++ src/LargestPerimeterTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * Description: https://leetcode.com/problems/largest-perimeter-triangle/
+ */
+
+public class LargestPerimeterTriangle {
+
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i] < nums[i - 1] + nums[i - 2]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0;
+    }
+}
