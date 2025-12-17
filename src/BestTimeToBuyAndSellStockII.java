3ac488de549c6bbeb349ded2ff2c3b8cddcd4158
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2025-12-17 15:50:27 +0700

    Leetcode 17/12/2025

---
+++ src/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,16 @@
+/**
+ * Description: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+ */
+
+public class BestTimeToBuyAndSellStockII {
+
+	public int maxProfit(int[] prices) {
+		int n = prices.length, ans = 0;
+		for (int i = 1; i < n; i++) {
+			if (prices[i-1] < prices[i]) {
+				ans += prices[i] - prices[i-1];
+			}
+		}
+		return ans;
+	}
+}
