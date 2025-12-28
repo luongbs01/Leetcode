7036655fac8acce8beb3e6afad09b1dfdda72211
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2025-12-28 09:52:16 +0700

    Leetcode daily 28/12/2025

--- src/CountNegativeNumbersInASortedMatrix.java
+++ src/CountNegativeNumbersInASortedMatrix.java
@@ -5,10 +5,9 @@
 public class CountNegativeNumbersInASortedMatrix {
 
 	public int countNegatives(int[][] grid) {
-		int m = grid.length, n = grid[0].length - 1, index = n, ans = 0;
-		// use enhanced for loop take 99ms to run all test cases??
-		for (int i = 0; i < m; i++) {
-			while (index >= 0 && grid[i][index] < 0) {
+		int n = grid[0].length - 1, index = n, ans = 0;
+		for (int[] row : grid) {
+			while (index >= 0 && row[index] < 0) {
 				index--;
 			}
 			ans += n - index;
