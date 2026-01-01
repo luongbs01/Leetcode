6bea86e618887033af495d6bfa0e66b20292d009
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-01-01 08:20:45 +0700

    Leetcode daily 01/01/2026

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -1,32 +1,26 @@
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 /**
  * Description: https://leetcode.com/problems/plus-one/description/
  */
 
 public class PlusOne {
-    public int[] plusOne(int[] digits) {
-        LinkedList<Integer> list = new LinkedList<>();
-        int m = 0;
-        int digit;
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (i == digits.length - 1) {
-                digit = digits[i] + 1;
-            } else {
-                digit = digits[i] + m;
-            }
-            list.addFirst(digit % 10);
-            m = digit / 10;
-        }
-        if (m == 1) {
-            list.addFirst(1);
-        }
-        int[] ans = new int[list.size()];
-        Iterator<Integer> iterator = list.iterator();
-        for (int i = 0; i < list.size(); i++) {
-            ans[i] = iterator.next();
-        }
-        return ans;
-    }
+	public int[] plusOne(int[] digits) {
+		ArrayList<Integer> list = new ArrayList<>(digits.length + 1);
+		int carry = 1;
+		int digit;
+		for (int i = digits.length - 1; i >= 0; i--) {
+			digit = digits[i] + carry;
+			list.add(digit % 10);
+			carry = digit / 10;
+		}
+		if (carry == 1) {
+			list.add(1);
+		}
+		int[] ans = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			ans[i] = list.get(list.size() - 1 - i);
+		}
+		return ans;
+	}
 }
