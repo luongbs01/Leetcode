e73f75397e8e306fa72ff6603ec872700a8a5128
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-03-09 22:21:45 +0700

    Leetcode daily 09/03/2026

---
+++ src/FindAllPossibleStableBinaryArraysI.java
@@ -0,0 +1,38 @@
+/**
+ * Description: https://leetcode.com/problems/find-all-possible-stable-binary-arrays-i/description/
+ */
+
+public class FindAllPossibleStableBinaryArraysI {
+
+	// consecutive constraint -> track last digit
+	public int numberOfStableArrays(int zero, int one, int limit) {
+		final int MOD = 1_000_000_007;
+
+		// dp[i][j][0]: number of stable array containing i zeros and j ones, and the last placed number is 0
+		// dp[i][j][1]: number of stable array containing i zeros and j ones, and the last placed number is 1
+		long[][][] dp = new long[zero + 1][one + 1][2];
+
+		for (int i = 1; i <= Math.min(zero, limit); i++) {
+			dp[i][0][0] = 1;
+		}
+		for (int i = 1; i <= Math.min(one, limit); i++) {
+			dp[0][i][1] = 1;
+		}
+
+		for (int i = 1; i <= zero; i++) {
+			for (int j = 1; j <= one; j++) {
+				dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+				if (i > limit) { // dp[i - 1 - limit][j][1] needs adding (limit+1) consecutive 0s to form dp[i][j][0] -> invalid
+					dp[i][j][0] = (dp[i][j][0] - dp[i - 1 - limit][j][1] + MOD) % MOD;
+				}
+
+				dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
+				if (j > limit) {
+					dp[i][j][1] = (dp[i][j][1] - dp[i][j - 1 - limit][0] + MOD) % MOD;
+				}
+			}
+		}
+
+		return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+	}
+}
