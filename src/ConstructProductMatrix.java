39f7f9b8205d66bbd84e8341f1fccda6835b8f74
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-03-24 09:18:40 +0700

    Leetcode daily 24/03/2026

--- src/ConstructProductMatrix.java
+++ src/ConstructProductMatrix.java
@@ -4,6 +4,7 @@
 
 public class ConstructProductMatrix {
 
+	// 26ms
 	public int[][] constructProductMatrix(int[][] grid) {
 		int m = grid.length, n = grid[0].length, mod = 12345;
 		int[][] ans = new int[m][n];
@@ -33,6 +34,35 @@ public class ConstructProductMatrix {
 				if (j > 0) p *= leftPrefix[i][j - 1];
 				if (j + 1 < n) p *= rightPrefix[i][j + 1];
 				ans[i][j] = (int) (p % mod);
+			}
+		}
+		return ans;
+	}
+
+	// 11ms
+	public int[][] constructProductMatrixV2(int[][] grid) {
+		int m = grid.length, n = grid[0].length, index = 0, mod = 12_345;
+		int[][] ans = new int[m][n];
+		long[] prefix = new long[m * n + 1];
+		long[] suffix = new long[m * n + 1];
+		prefix[0] = 1;
+		suffix[m * n] = 1;
+		for (int[] row : grid) {
+			for (int j = 0; j < n; j++) {
+				prefix[index + 1] = (prefix[index] * row[j]) % mod;
+				index++;
+			}
+		}
+		for (int i = m - 1; i >= 0; i--) {
+			for (int j = n - 1; j >= 0; j--) {
+				suffix[index - 1] = (suffix[index] * grid[i][j]) % mod;
+				index--;
+			}
+		}
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				ans[i][j] = (int) ((prefix[index] * suffix[index + 1]) % mod);
+				index++;
 			}
 		}
 		return ans;
