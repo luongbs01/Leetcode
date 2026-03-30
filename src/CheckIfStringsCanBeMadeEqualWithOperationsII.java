2b2d2eb3a653e1cdcde87e3b75f981ecf7df3c82
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-03-30 09:00:11 +0700

    Leetcode daily 30/03/2026

---
+++ src/CheckIfStringsCanBeMadeEqualWithOperationsII.java
@@ -0,0 +1,20 @@
+/**
+ * Description: https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/
+ */
+
+public class CheckIfStringsCanBeMadeEqualWithOperationsII {
+
+	public boolean checkStrings(String s1, String s2) {
+		int[][] freq = new int[2][26];
+		for (int i = 0; i < s1.length(); i++) {
+			freq[i % 2][s1.charAt(i) - 'a']++;
+			freq[i % 2][s2.charAt(i) - 'a']--;
+		}
+		for (int i = 0; i < 26; i++) {
+			if (freq[0][i] != 0 || freq[1][i] != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
