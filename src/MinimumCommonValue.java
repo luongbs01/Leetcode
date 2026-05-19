d46ba1454de0195e707155bf213b0890fd5f0121
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-05-19 09:59:59 +0700

    Leetcode daily 19/05/2026

---
+++ src/MinimumCommonValue.java
@@ -0,0 +1,20 @@
+/**
+ * Description: https://leetcode.com/problems/minimum-common-value/
+ */
+
+public class MinimumCommonValue {
+
+	public int getCommon(int[] nums1, int[] nums2) {
+		int n = nums1.length, m = nums2.length, i = 0, j = 0;
+		while (i < n && j < m) {
+			if (nums1[i] > nums2[j]) {
+				j++;
+			} else if (nums1[i] < nums2[j]) {
+				i++;
+			} else {
+				return nums1[i];
+			}
+		}
+		return -1;
+	}
+}
