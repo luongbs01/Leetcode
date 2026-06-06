ab7798bb28b47d923ce07f63c29ca65f482d4ebe
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-06-06 08:52:04 +0700

    Leetcode daily 06/06/2026

--- src/LeftAndRightSumDifferences.java
+++ src/LeftAndRightSumDifferences.java
@@ -5,17 +5,16 @@
 public class LeftAndRightSumDifferences {
 
     public int[] leftRightDifference(int[] nums) {
-        int n = nums.length;
-        int[] leftSum = new int[n], rightSum = new int[n], ans = new int[n];
+        int n = nums.length, rightSum = 0;
+        int[] leftSum = new int[n], ans = new int[n];
         leftSum[0] = 0;
         for (int i = 1; i < n; i++) {
             leftSum[i] = leftSum[i - 1] + nums[i - 1];
         }
-        rightSum[n - 1] = 0;
-        ans[n - 1] = Math.abs(leftSum[n - 1] - rightSum[n - 1]);
+        ans[n - 1] = Math.abs(leftSum[n - 1] - rightSum);
         for (int i = n - 2; i >= 0; i--) {
-            rightSum[i] = rightSum[i + 1] + nums[i + 1];
-            ans[i] = Math.abs(leftSum[i] - rightSum[i]);
+            rightSum += nums[i + 1];
+            ans[i] = Math.abs(leftSum[i] - rightSum);
         }
         return ans;
     }
