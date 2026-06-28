95ba6281acde73618b746eb6b30bec8a3b628b6d
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-06-28 07:43:32 +0700

    Leetcode daily 28/06/2026

---
+++ src/MaximumElementAfterDecreasingAndRearranging.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+/**
+ * Description: https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+ */
+
+public class MaximumElementAfterDecreasingAndRearranging {
+
+	public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+		Arrays.sort(arr);
+		arr[0] = 1;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > arr[i - 1] + 1) {
+				arr[i] = arr[i - 1] + 1;
+			}
+		}
+		return arr[arr.length - 1];
+	}
+}
