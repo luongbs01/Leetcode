6b2476fd6adaccc69418081cbd0aa36c7ea35ea9
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-07-26 14:55:17 +0700

    Leetcode daily 26/07/2026

---
+++ src/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+/**
+ * Description: https://leetcode.com/problems/maximum-product-of-three-numbers/
+ */
+
+public class MaximumProductOfThreeNumbers {
+
+	public int maximumProduct(int[] nums) {
+		int n = nums.length;
+		Arrays.sort(nums);
+		return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+	}
+}
