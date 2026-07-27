4bdb3da0fd07a1ea970b80797d127f68790a6425
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-07-27 09:36:36 +0700

    Leetcode daily 27/07/2026

---
+++ src/MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+/**
+ * Description: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+ */
+
+public class MaximumProductOfTwoElementsInAnArray {
+
+	public int maxProduct(int[] nums) {
+		int n = nums.length;
+		Arrays.sort(nums);
+		return (nums[n - 2] - 1) * (nums[n - 1] - 1);
+	}
+}
