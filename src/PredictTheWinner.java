9213904a5796e24ff99c8a450416069dc2359e35
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-08-02 10:37:23 +0700

    Leetcode daily 01/08/2026

--- src/PredictTheWinner.java
+++ src/PredictTheWinner.java
@@ -6,13 +6,13 @@ public class PredictTheWinner {
 
 	public boolean predictTheWinner(int[] nums) {
 		int n = nums.length;
-		// dp[i][j]: maximum diff between player 1 and player 2 when player 1 takes first on subarray [i..j]
+		// dp[i][j]: maximum score difference the current player can achieve over the opponent while playing on subarray [i..j]
 		int[][] dp = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			dp[i][i] = nums[i];
 		}
 		for (int dist = 1; dist < n; dist++) {
-			for (int i = 0; i < n - dist; i++) {
+			for (int i = 0; i + dist < n; i++) {
 				dp[i][i + dist] = Math.max(nums[i] - dp[i + 1][i + dist], nums[i + dist] - dp[i][i + dist - 1]);
 			}
 		}
