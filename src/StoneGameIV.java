39b9d7d195fcea4fafe5efcfc1d81e552f501c9c
Lương Minh Hoang <luong.minh.hoang@mgm-tp.com>
2026-08-10 10:35:42 +0700

    Leetcode daily 10/08/2026

--- src/StoneGameIV.java
+++ src/StoneGameIV.java
@@ -9,14 +9,12 @@ public class StoneGameIV {
 		boolean[] dp = new boolean[n + 1];
 		dp[0] = dp[1] = true;
 		for (int i = 2; i <= n; i++) {
-			boolean canWin = false;
 			for (int j = 1; j * j <= i; j++) {
 				if (j * j == i || !dp[i - j * j]) {
-					canWin = true;
+					dp[i] = true;
 					break;
 				}
 			}
-			dp[i] = canWin;
 		}
 		return dp[n];
 	}
