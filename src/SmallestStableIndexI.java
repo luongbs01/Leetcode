861eaffa1a6182805d3974130293dd9e5f36a9dd
Luong Hoang <luongbs01@gmail.com>
2026-09-04 07:23:21 +0700

    Leetcode daily 04/09/2026

---
+++ src/SmallestStableIndexI.java
@@ -0,0 +1,22 @@
+/**
+ * Description: https://leetcode.com/problems/smallest-stable-index-i/
+ */
+
+public class SmallestStableIndexI {
+
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length, preMax = 0;
+        int[] sufMin = new int[n];
+        sufMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            sufMin[i] = Math.min(sufMin[i + 1], nums[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            preMax = Math.max(preMax, nums[i]);
+            if (preMax - sufMin[i] <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
