3cb23e775be08b420211e4c1c07a164acca37eca
luong.hoang2-it-platform <luong.hoang2@galaxyfinx.com>
2026-09-13 22:16:47 +0700

    Leetcode daily 13/09/2026

---
+++ src/ImageOverlap.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description: https://leetcode.com/problems/maximum-score-of-non-overlapping-intervals/
+ */
+
+public class ImageOverlap {
+
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        List<int[]> A = new ArrayList<>();
+        List<int[]> B = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (img1[i][j] == 1) A.add(new int[]{i, j});
+                if (img2[i][j] == 1) B.add(new int[]{i, j});
+            }
+        }
+        int[][] cnt = new int[2 * n][2 * n];
+        int best = 0;
+        for (int[] a : A) {
+            for (int[] b : B) {
+                int dx = b[0] - a[0] + n;
+                int dy = b[1] - a[1] + n;
+                cnt[dx][dy]++;
+                best = Math.max(best, cnt[dx][dy]);
+            }
+        }
+        return best;
+    }
+}
